46fc6feb79642a9c9150014ed68f4e82a23d1bc5
Floess, Constantin <cfloess@lcs-mba3343.local>
2017-10-26 11:00:35 -0400

    first steps for bouncing circle

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,6 +9,9 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     
+    //cange in postion of circle
+    var dx : Int
+    
     // This function runs once
     override init() {
         
@@ -18,17 +21,35 @@ class Sketch : NSObject {
         // Set starting position
         x = 250
         
+        // Set change value
+        dx = 2
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
+        //clean up
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
+        
         // Change position
-        x += 1
+        x += dx
+        
+        //make the circle bounce on the right edge
+        
+        if x > 500{
+            dx = -2
+        }
+        
+        if x < 0 {
+            dx = 2
+        }
         
         // Draw an ellipse in the middle of the canvas
+        canvas.fillColor = Color.black
         canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
         
     }
     
 }
+
